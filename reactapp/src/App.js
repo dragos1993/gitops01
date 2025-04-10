f746e2aeaec0e3784d12d7095949518612053739
dragos1993 <vrinceanu.dragos24@yahoo.com>
2025-04-10 18:09:14 +0300

    test

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-link">Learn Docker with React.js3</h1>
+        <h1 className="App-link">Learn Docker with React.js4</h1>
       </header>
     </div>
   );
